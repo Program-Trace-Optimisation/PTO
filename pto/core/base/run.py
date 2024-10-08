a0f672aaf090244f042597eefbeef3a433b345b0
Alberto Moraglio <albmor@gmail.com>
2024-10-08 01:08:54 +0100

    Added 'search_operators' config option to 'run'

--- pto/core/base/run.py
+++ pto/core/base/run.py
@@ -5,7 +5,18 @@ from .trace_operators import Op
 class CoreRunner:
     @classmethod
     def run(cls, Gen, Fit, gen_args=(), fit_args=(), better=max, Solver='hill_climber', solver_args={}, callback=None):
-        
+                
+        # specialise generator and fitness (problem)
+        def gen(): return Gen(*gen_args)
+        def fit(sol): return Fit(sol, *fit_args)
+    
+        # bind search operators to problem
+        op = Op(generator=gen, fitness=fit)
+
+        # search opertaors mode
+        if Solver == 'search_operators':
+            return op
+
         # import the solver dynamically
         if isinstance(Solver, str):
             try:
@@ -13,13 +24,6 @@ class CoreRunner:
                 Solver = getattr(solver_module, Solver)
             except (ImportError, AttributeError):
                 raise ValueError(f"Unable to import solver '{Solver}'. Make sure it exists in the solvers module.")
-        
-        # specialise generator and fitness (problem)
-        def gen(): return Gen(*gen_args)
-        def fit(sol): return Fit(sol, *fit_args)
-    
-        # bind search operators to problem
-        op = Op(generator=gen, fitness=fit)
     
         # instantiate search algorithm and bind it to search operators
         alg = Solver(op, better=better, callback=callback, **solver_args)
