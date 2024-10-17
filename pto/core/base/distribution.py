0a64357322774fb182abb62a29ca83fe38970fea
Alberto Moraglio <albmor@gmail.com>
2024-10-17 15:38:47 +0100

    refactored distribution.py

--- pto/core/base/distribution.py
+++ pto/core/base/distribution.py
@@ -4,36 +4,32 @@ import random
 
 from .check_immutable import check_immutable
 
-class Dist(): # base class
+class Dist():
   
     def __init__(self, fun, *args, val = None):
         self.fun = fun
-        self.args = tuple(copy(arg) for arg in args) # args
+        self.args = tuple(copy(arg) for arg in args)
         self.val = val
     
-    def sample(self): # alter self
+    def sample(self):
         self.val = self.fun(*self.args)
 
-    def repair(self, _): # alter self
+    def repair(self, _):
         self.sample()
     
     @check_immutable 
-    def mutation(self): # does not alter self
+    def mutation(self):
         offspring = copy(self)
         offspring.sample()
         return offspring
     
     @check_immutable 
-    def crossover(self, other): # does not alter self and other
-        offspring = random.choice([self, other])
-        #offspring = copy(offspring)
-        return offspring
+    def crossover(self, other):
+        return random.choice([self, other])
     
     @check_immutable 
-    def convex_crossover(self, other1, other2): # does not alter self and other
-        offspring = random.choice([self, other1, other2])
-        #offspring = copy(offspring)
-        return offspring
+    def convex_crossover(self, other1, other2):
+        return random.choice([self, other1, other2])
     
     @check_immutable
     def distance(self, other):
@@ -41,7 +37,8 @@ class Dist(): # base class
     
     def __repr__(self):
         return f'{self.__class__.__name__}({self.fun.__name__}, {self.args}, val={self.val})' 
-        #return self.__class__.__name__ + str((self.fun.__name__, self.args, self.val))
-
+    
     def __eq__(self, other):
-        return (self.fun.__name__ == other.fun.__name__) and (self.args == other.args) and (self.val == other.val)
+        return (self.fun.__name__ == other.fun.__name__ and 
+                self.args == other.args and 
+                self.val == other.val)
