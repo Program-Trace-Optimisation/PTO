1ab68f2f4e940f99ccf7b41ab952a88103552057
Alberto Moraglio <albmor@gmail.com>
2025-01-05 17:52:36 +0000

    added docstring

--- pto/core/automatic_names/run.py
+++ pto/core/automatic_names/run.py
@@ -5,12 +5,35 @@ from ..fine_distributions import run as fine_run
 from .autoplay import tracer
 from .autogens import rnd
 
-def run(*args, name_type = 'str', dist_type = 'fine', **kwargs):
-    # name_type can be 'lin' or 'str'
-    # dist_type can be 'coarse' or 'fine'
+def run(*args, name_type='str', dist_type='fine', **kwargs):
+    """
+    Configure naming and distribution types, then run optimization.
     
-    rnd.CONFIG(name_type = name_type, dist_type = dist_type)
-    Op.tracer = tracer # binds autoplay tracer to operators
+    Sets up the execution environment with specified naming and distribution
+    strategies before delegating to the fine_run implementation.
     
-    return fine_run(*args, dist_type = None, **kwargs)
-
+    Args:
+        *args: Positional arguments to pass to fine_run
+        name_type: How to generate names - 'lin' (sequential) or 'str' (structural)
+        dist_type: Type of distribution - 'coarse' or 'fine'
+        **kwargs: Additional arguments to pass to fine_run
+        
+    Returns:
+        Result from fine_run execution
+        
+    Example:
+        # Run with structural naming and fine distributions
+        solution = run(generator, fitness, name_type='str', dist_type='fine')
+        
+        # Run with sequential naming
+        solution = run(generator, fitness, name_type='lin')
+    """
+    # Configure random number generator naming and distribution types
+    rnd.CONFIG(name_type=name_type, dist_type=dist_type)
+    
+    # Bind autoplay tracer to operators
+    Op.tracer = tracer
+    
+    # Run optimization with configured environment
+    # Pass dist_type=None since already configured in rnd
+    return fine_run(*args, dist_type=None, **kwargs)
