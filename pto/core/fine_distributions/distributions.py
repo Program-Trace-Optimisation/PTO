5d8d1fff7315a958895229156a5516bec656847b
Alberto Moraglio <albmor@gmail.com>
2025-01-05 14:31:19 +0000

    handle kwargs in fine distributions

--- pto/core/fine_distributions/distributions.py
+++ pto/core/fine_distributions/distributions.py
@@ -4,223 +4,210 @@ import random
 from ..base import Dist, check_immutable
 from .supp import rng_specs
 
-#---
-
 class Random_real(Dist):
-    def __init__(self, fun, *args, val=None):
-        super().__init__(fun, *args, val=val)
-        self.min, self.max, self.range = rng_specs[fun].params(args)
-    
-    def repair_val(self):
-        self.val = min(max(self.min, self.val), self.max)
-    
-    @check_immutable
-    def mutation(self):
-        offspring = copy(self)
-        offspring.val = self.val + random.gauss(0, 0.1*self.range)
-        offspring.repair_val()
-        return offspring
+   """Handle continuous real-valued random variables within a range."""
+   def __init__(self, fun, *args, val=None, **kwargs):
+       super().__init__(fun, *args, val=val, **kwargs)
+       self.min, self.max, self.range = rng_specs[fun].params(args, kwargs)
+   
+   def repair_val(self):
+       self.val = min(max(self.min, self.val), self.max)
+   
+   @check_immutable
+   def mutation(self):
+       offspring = copy(self)
+       offspring.val = self.val + random.gauss(0, 0.1*self.range)
+       offspring.repair_val()
+       return offspring
 
-    @check_immutable
-    def crossover(self, other):
-        if isinstance(other, Random_real):
-            offspring = copy(self)
-            offspring.val = random.uniform(self.val, other.val)
-            return offspring
-        return super().crossover(other)
-    
-    @check_immutable
-    def convex_crossover(self, other1, other2):
-        if isinstance(other1, Random_real) and isinstance(other2, Random_real):
-            offspring = copy(self)
-            min_val = min(self.val, other1.val, other2.val)
-            max_val = max(self.val, other1.val, other2.val)
-            offspring.val = random.uniform(min_val, max_val)
-            return offspring
-        return super().convex_crossover(other1, other2)
+   @check_immutable
+   def crossover(self, other):
+       if isinstance(other, Random_real):
+           offspring = copy(self)
+           offspring.val = random.uniform(self.val, other.val)
+           return offspring
+       return super().crossover(other)
+   
+   @check_immutable
+   def convex_crossover(self, other1, other2):
+       if isinstance(other1, Random_real) and isinstance(other2, Random_real):
+           offspring = copy(self)
+           min_val = min(self.val, other1.val, other2.val)
+           max_val = max(self.val, other1.val, other2.val)
+           offspring.val = random.uniform(min_val, max_val)
+           return offspring
+       return super().convex_crossover(other1, other2)
 
-    @check_immutable
-    def distance(self, other):
-        if isinstance(other, Random_real):
-            return min(1, abs(self.val - other.val)/self.range)
-        return super().distance(other)
-    
-    def size(self):
-        return 10
-    
-    def repair(self, other):
-        if isinstance(other, Random_real):
-            self.val = ((other.val-other.min)/other.range)*self.range+self.min
-            self.repair_val()
-        else:
-            super().repair(other)
-
-#---
+   @check_immutable
+   def distance(self, other):
+       if isinstance(other, Random_real):
+           return min(1, abs(self.val - other.val)/self.range)
+       return super().distance(other)
+   
+   def size(self):
+       return 10
+   
+   def repair(self, other):
+       if isinstance(other, Random_real):
+           self.val = ((other.val-other.min)/other.range)*self.range+self.min
+           self.repair_val()
+       else:
+           super().repair(other)
 
 class Random_int(Dist):
-    def __init__(self, fun, *args, val=None):
-        super().__init__(fun, *args, val=val)
-        self.min, self.max, self.step = rng_specs[fun].params(args)
-    
-    def repair_val(self):
-        round_val = self.min + round((self.val - self.min) / self.step) * self.step
-        self.val = min(max(self.min, round_val), self.max)
-    
-    @check_immutable
-    def mutation(self):
-        offspring = copy(self)
-        for _ in range(10):
-            offspring.val = self.val + random.choice([-1, 1]) * self.step
-            offspring.repair_val()
-            if offspring.val != self.val: break
-        return offspring
+   """Handle discrete integer variables with step size."""
+   def __init__(self, fun, *args, val=None, **kwargs):
+       super().__init__(fun, *args, val=val, **kwargs)
+       self.min, self.max, self.step = rng_specs[fun].params(args, kwargs)
+   
+   def repair_val(self):
+       round_val = self.min + round((self.val - self.min) / self.step) * self.step
+       self.val = min(max(self.min, round_val), self.max)
+   
+   @check_immutable
+   def mutation(self):
+       offspring = copy(self)
+       for _ in range(10):
+           offspring.val = self.val + random.choice([-1, 1]) * self.step
+           offspring.repair_val()
+           if offspring.val != self.val: break
+       return offspring
 
-    @check_immutable
-    def crossover(self, other):
-        if isinstance(other, Random_int):
-            offspring = copy(self)
-            min_val = min(self.val, other.val)
-            max_val = max(self.val, other.val)
-            offspring.val = random.randint(min_val, max_val)
-            return offspring
-        return super().crossover(other)
-    
-    @check_immutable
-    def convex_crossover(self, other1, other2):
-        if isinstance(other1, Random_int) and isinstance(other2, Random_int):
-            offspring = copy(self)
-            min_val = min(self.val, other1.val, other2.val)
-            max_val = max(self.val, other1.val, other2.val)
-            offspring.val = random.randint(min_val, max_val)
-            return offspring
-        return super().convex_crossover(other1, other2)
+   @check_immutable
+   def crossover(self, other):
+       if isinstance(other, Random_int):
+           offspring = copy(self)
+           min_val = min(self.val, other.val)
+           max_val = max(self.val, other.val)
+           offspring.val = random.randint(min_val, max_val)
+           return offspring
+       return super().crossover(other)
+   
+   @check_immutable
+   def convex_crossover(self, other1, other2):
+       if isinstance(other1, Random_int) and isinstance(other2, Random_int):
+           offspring = copy(self)
+           min_val = min(self.val, other1.val, other2.val)
+           max_val = max(self.val, other1.val, other2.val)
+           offspring.val = random.randint(min_val, max_val)
+           return offspring
+       return super().convex_crossover(other1, other2)
 
-    @check_immutable
-    def distance(self, other):
-        if isinstance(other, Random_int):
-            return min(1, abs(self.val - other.val)/(self.max-self.min))
-        return super().distance(other)
-    
-    def size(self):
-        return (self.max-self.min)/self.step
+   @check_immutable
+   def distance(self, other):
+       if isinstance(other, Random_int):
+           return min(1, abs(self.val - other.val)/(self.max-self.min))
+       return super().distance(other)
+   
+   def size(self):
+       return (self.max-self.min)/self.step
 
-    def repair(self, other):
-        if isinstance(other, Random_int):
-            self.val = ((other.val-other.min)/other.step)*self.step+self.min
-            self.repair_val()
-        else:
-            super().repair(other)
-
-#---
+   def repair(self, other):
+       if isinstance(other, Random_int):
+           self.val = ((other.val-other.min)/other.step)*self.step+self.min
+           self.repair_val()
+       else:
+           super().repair(other)
 
 class Random_cat(Dist):
-    def __init__(self, fun, *args, val=None):
-        super().__init__(fun, *args, val=val)
-        self.seq = rng_specs[fun].params(args)
-        #(self.seq)
-        #self.args = (copy(self.args[0]),)
-    
-    def repair_val(self):
-        if self.val not in self.seq:
-            self.val = random.choice(self.seq)
+   """Handle categorical variables from a fixed sequence."""
+   def __init__(self, fun, *args, val=None, **kwargs):
+       super().__init__(fun, *args, val=val, **kwargs)
+       self.seq = rng_specs[fun].params(args, kwargs)
+   
+   def repair_val(self):
+       if self.val not in self.seq:
+           self.val = random.choice(self.seq)
 
-    @check_immutable
-    def mutation(self):
-        offspring = copy(self)
-        if len(set(self.seq)) >= 2:
-            offspring.val = random.choice([v for v in self.seq if v != self.val])
-        return offspring
-    
-    def size(self):
-        return len(self.seq)
+   @check_immutable
+   def mutation(self):
+       offspring = copy(self)
+       if len(set(self.seq)) >= 2:
+           offspring.val = random.choice([v for v in self.seq if v != self.val])
+       return offspring
+   
+   def size(self):
+       return len(self.seq)
 
-    def repair(self, other):
-        if isinstance(other, Random_cat):
-            if other.val in self.seq:
-                self.val = other.val
-            else:
-                diff_seq = [val for val in self.seq if val not in other.seq]
-                self.val = random.choice(diff_seq) if diff_seq else random.choice(self.seq)
-        else:
-            super().repair(other)
-
-#---
-
-# TODO: 1) check implementation and test this class
-#       2) implement handling of keyword args and inplace args (DONE)
+   def repair(self, other):
+       if isinstance(other, Random_cat):
+           if other.val in self.seq:
+               self.val = other.val
+           else:
+               diff_seq = [val for val in self.seq if val not in other.seq]
+               self.val = random.choice(diff_seq) if diff_seq else random.choice(self.seq)
+       else:
+           super().repair(other)
 
 class Random_seq(Dist):
-    def __init__(self, fun, *args, val=None):
-        super().__init__(fun, *args, val=val)
-        self.sequence, self.k = rng_specs[fun].params(args)
-        
-    def repair_val(self):
-        if not self.val or len(self.val) != self.k:
-            self.val = random.sample(self.sequence, self.k)
-    
-    @check_immutable
-    def mutation(self):
-        offspring = copy(self)
-        if len(self.sequence) >= 2:
-            idx = random.randrange(len(offspring.val))
-            valid_choices = [x for x in self.sequence if x not in offspring.val]
-            if valid_choices:
-                offspring.val = list(offspring.val)
-                offspring.val[idx] = random.choice(valid_choices)
-                offspring.val = tuple(offspring.val)
-        return offspring
-    
-    @check_immutable
-    def crossover(self, other):
-        if isinstance(other, Random_seq):
-            offspring = copy(self)
-            if len(set(self.sequence)) >= 2:
-                # Take some elements from each parent while maintaining uniqueness
-                combined = list(set(self.val) | set(other.val))
-                if len(combined) >= self.k:
-                    offspring.val = tuple(random.sample(combined, self.k))
-                else:
-                    # If we need more elements, sample from remaining sequence
-                    needed = self.k - len(combined)
-                    remaining = [x for x in self.sequence if x not in combined]
-                    additional = random.sample(remaining, needed)
-                    offspring.val = tuple(combined + additional)
-            return offspring
-        return super().crossover(other)
+   """Handle sequences of k unique items from a larger sequence."""
+   def __init__(self, fun, *args, val=None, **kwargs):
+       super().__init__(fun, *args, val=val, **kwargs)
+       self.sequence, self.k = rng_specs[fun].params(args, kwargs)
+       
+   def repair_val(self):
+       if not self.val or len(self.val) != self.k:
+           self.val = random.sample(self.sequence, self.k)
+   
+   @check_immutable
+   def mutation(self):
+       offspring = copy(self)
+       if len(self.sequence) >= 2:
+           idx = random.randrange(len(offspring.val))
+           valid_choices = [x for x in self.sequence if x not in offspring.val]
+           if valid_choices:
+               offspring.val = list(offspring.val)
+               offspring.val[idx] = random.choice(valid_choices)
+               offspring.val = tuple(offspring.val)
+       return offspring
+   
+   @check_immutable
+   def crossover(self, other):
+       if isinstance(other, Random_seq):
+           offspring = copy(self)
+           if len(set(self.sequence)) >= 2:
+               combined = list(set(self.val) | set(other.val))
+               if len(combined) >= self.k:
+                   offspring.val = tuple(random.sample(combined, self.k))
+               else:
+                   needed = self.k - len(combined)
+                   remaining = [x for x in self.sequence if x not in combined]
+                   additional = random.sample(remaining, needed)
+                   offspring.val = tuple(combined + additional)
+           return offspring
+       return super().crossover(other)
 
-    @check_immutable
-    def convex_crossover(self, other1, other2):
-        if isinstance(other1, Random_seq) and isinstance(other2, Random_seq):
-            offspring = copy(self)
-            if len(set(self.sequence)) >= 2:
-                # Combine elements from all three parents
-                combined = list(set(self.val) | set(other1.val) | set(other2.val))
-                if len(combined) >= self.k:
-                    offspring.val = tuple(random.sample(combined, self.k))
-                else:
-                    # If we need more elements, sample from remaining sequence
-                    needed = self.k - len(combined)
-                    remaining = [x for x in self.sequence if x not in combined]
-                    additional = random.sample(remaining, needed)
-                    offspring.val = tuple(combined + additional)
-            return offspring
-        return super().convex_crossover(other1, other2)
-    
-    def size(self):
-        return min(len(self.sequence), self.k)
-    
-    def repair(self, other):
-        if isinstance(other, Random_seq):
-            if len(other.val) == self.k:
-                preserved = [x for x in other.val if x in self.sequence]
-                needed = self.k - len(preserved)
-                if needed > 0:
-                    available = [x for x in self.sequence if x not in preserved]
-                    additional = random.sample(available, min(needed, len(available)))
-                    self.val = tuple(preserved + additional)
-                else:
-                    self.val = tuple(preserved[:self.k])
-            else:
-                self.val = random.sample(self.sequence, self.k)
-        else:
-            super().repair(other)
+   @check_immutable
+   def convex_crossover(self, other1, other2):
+       if isinstance(other1, Random_seq) and isinstance(other2, Random_seq):
+           offspring = copy(self)
+           if len(set(self.sequence)) >= 2:
+               combined = list(set(self.val) | set(other1.val) | set(other2.val))
+               if len(combined) >= self.k:
+                   offspring.val = tuple(random.sample(combined, self.k))
+               else:
+                   needed = self.k - len(combined)
+                   remaining = [x for x in self.sequence if x not in combined]
+                   additional = random.sample(remaining, needed)
+                   offspring.val = tuple(combined + additional)
+           return offspring
+       return super().convex_crossover(other1, other2)
+   
+   def size(self):
+       return min(len(self.sequence), self.k)
+   
+   def repair(self, other):
+       if isinstance(other, Random_seq):
+           if len(other.val) == self.k:
+               preserved = [x for x in other.val if x in self.sequence]
+               needed = self.k - len(preserved)
+               if needed > 0:
+                   available = [x for x in self.sequence if x not in preserved]
+                   additional = random.sample(available, min(needed, len(available)))
+                   self.val = tuple(preserved + additional)
+               else:
+                   self.val = tuple(preserved[:self.k])
+           else:
+               self.val = random.sample(self.sequence, self.k)
+       else:
+           super().repair(other)
--- pto/core/fine_distributions/supp.py
+++ pto/core/fine_distributions/supp.py
@@ -13,106 +13,180 @@ Parameter formats by type:
 
 RNGSpec = namedtuple('RNGSpec', ['type', 'params'])
 
-rng_specs = {
-   # Real-valued functions - all return (min, max, range)
-   random.random: RNGSpec(
-       type='real',
-       params=lambda args: (0, 1, 1)
-   ),
-   random.uniform: RNGSpec(
-       type='real',
-       params=lambda args: (args[0], args[1], args[1]-args[0])
-   ),
-   random.triangular: RNGSpec(
-       type='real',
-       params=lambda args: (args[0], args[1], args[1]-args[0])
-   ),
-   random.betavariate: RNGSpec(
-       type='real',
-       params=lambda args: (0, 1, 1)  # Always [0,1]
-   ),
-   random.expovariate: RNGSpec(
-       type='real',
-       params=lambda args: (0, math.inf, 1.0/args[0] * 2)  # std_dev = 1/lambda
-   ),
-   random.gammavariate: RNGSpec(
-       type='real',
-       params=lambda args: (0, math.inf, args[0]**0.5/args[1] * 2)  # std_dev = sqrt(alpha)/beta
-   ),
-   random.gauss: RNGSpec(
-       type='real',
-       params=lambda args: (-math.inf, math.inf, args[1] * 2)  # range = 2*sigma
-   ),
-   random.lognormvariate: RNGSpec(
-       type='real',
-       params=lambda args: (0, math.inf, 
-           (math.exp(args[1]**2-1)*math.exp(2*args[0]+args[1]**2))**0.5 * 2)  # 2*std_dev
-   ),
-   random.normalvariate: RNGSpec(
-       type='real',
-       params=lambda args: (-math.inf, math.inf, args[1] * 2)  # range = 2*sigma
-   ),
-   random.vonmisesvariate: RNGSpec(
-       type='real',
-       params=lambda args: (-math.pi, math.pi, math.pi * 2)  # Circular distribution [-π,π]
-   ),
-   random.paretovariate: RNGSpec(
-       type='real',
-       params=lambda args: (1, math.inf,  # Always starts at 1
-           (args[0]/((args[0]-1)**2 * (args[0]-2)))**0.5 * 2)  # 2*std_dev
-   ),
-   random.weibullvariate: RNGSpec(
-       type='real',
-       params=lambda args: (0, math.inf,
-           1.0/args[1] * (math.gamma(1+2.0/args[0])-math.gamma(1+1.0/args[0])**2)**0.5 * 2)  # 2*std_dev
-   ),
+class RNGSpecs:
+   """
+   Specifications for random number generators, handling both positional and keyword arguments.
+   Each RNG function has a type and a parameter extraction method.
+   """
+   
+   def __init__(self):
+       """Initialize the RNG specifications dictionary."""
+       self.specs = {
+           # Real-valued functions
+           random.random: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (0, 1, 1)
+           ),
+           random.uniform: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (
+                   args[0] if args else kwargs['a'],
+                   args[1] if len(args) > 1 else kwargs['b'],
+                   (args[1] if len(args) > 1 else kwargs['b']) - 
+                   (args[0] if args else kwargs['a'])
+               )
+           ),
+           random.triangular: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (
+                   args[0] if args else kwargs['low'],
+                   args[1] if len(args) > 1 else kwargs['high'],
+                   (args[1] if len(args) > 1 else kwargs['high']) - 
+                   (args[0] if args else kwargs['low'])
+               )
+           ),
+           random.betavariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (0, 1, 1)
+           ),
+           random.gauss: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (-math.inf, math.inf, 
+                   2 * (args[1] if len(args) > 1 else kwargs['sigma']))
+           ),
+           random.normalvariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (-math.inf, math.inf, 
+                   2 * (args[1] if len(args) > 1 else kwargs['sigma']))
+           ),
+           random.expovariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (0, math.inf, 
+                   2.0/(args[0] if args else kwargs['lambd']))
+           ),
+           random.gammavariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (0, math.inf, 
+                   2 * ((args[0] if args else kwargs['alpha'])**0.5 / 
+                        (args[1] if len(args) > 1 else kwargs['beta'])))
+           ),
+           random.lognormvariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (0, math.inf, 
+                   2 * (math.exp((args[1] if len(args) > 1 else kwargs['sigma'])**2 - 1) * 
+                        math.exp(2 * (args[0] if args else kwargs['mu']) + 
+                               (args[1] if len(args) > 1 else kwargs['sigma'])**2))**0.5)
+           ),
+           random.vonmisesvariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (-math.pi, math.pi, 2*math.pi)
+           ),
+           random.paretovariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (1, math.inf, 
+                   2 * ((args[0] if args else kwargs['alpha'])/
+                        ((args[0] if args else kwargs['alpha']-1)**2 * 
+                         (args[0] if args else kwargs['alpha']-2)))**0.5)
+           ),
+           random.weibullvariate: RNGSpec(
+               type='real',
+               params=lambda args, kwargs: (0, math.inf,
+                   2 * (1.0/(args[1] if len(args) > 1 else kwargs['beta']) * 
+                        (math.gamma(1 + 2.0/(args[0] if args else kwargs['alpha'])) - 
+                         math.gamma(1 + 1.0/(args[0] if args else kwargs['alpha']))**2)**0.5))
+           ),
+           
+           # Integer-valued functions
+           random.randrange: RNGSpec(
+               type='int',
+               params=lambda args, kwargs: 
+                   (0, args[0]-1, 1) if len(args) == 1 else
+                   (args[0], args[1]-1, args[2] if len(args) > 2 else 1) if args else
+                   (kwargs.get('start', 0), kwargs['stop']-1, kwargs.get('step', 1))
+           ),
+           random.randint: RNGSpec(
+               type='int',
+               params=lambda args, kwargs: (
+                   args[0] if args else kwargs['a'],
+                   args[1] if len(args) > 1 else kwargs['b'],
+                   1
+               )
+           ),
+           
+           # Categorical/Sequence functions
+           random.choice: RNGSpec(
+               type='cat',
+               params=lambda args, kwargs: args[0] if args else kwargs['seq']
+           ),
+           random.choices: RNGSpec(
+               type='seq',
+               params=lambda args, kwargs: (
+                   args[0] if args else kwargs['population'],
+                   kwargs.get('k', 1) if 'k' in kwargs else args[3] if len(args) > 3 else 1
+               )
+           ),
+           random.sample: RNGSpec(
+               type='seq',
+               params=lambda args, kwargs: (
+                   args[0] if args else kwargs['population'],
+                   args[1] if len(args) > 1 else kwargs['k']
+               )
+           )
+       }
+       
+   def __getitem__(self, key):
+       """Access specifications using dictionary syntax."""
+       return self.specs[key]
 
-   # Integer-valued functions - all return (min, max, step)
-   random.randrange: RNGSpec(
-       type='int',
-       params=lambda args: (0, args[0]-1, 1) if len(args)==1 else 
-                         (args[0], args[1]-1, 1) if len(args)==2 else 
-                         (args[0], args[1]-1, args[2])
-   ),
-   random.randint: RNGSpec(
-       type='int',
-       params=lambda args: (args[0], args[1], 1)
-   ),
+# Create global instance
+rng_specs = RNGSpecs()
 
-   # Categorical function - all return sequence
-   random.choice: RNGSpec(
-       type='cat',
-       params=lambda args: args[0]
-   ),
-
-   # Sequence functions - all return (sequence, k)
-   random.sample: RNGSpec(
-       type='seq',
-       params=lambda args: (args[0], args[1])
-   )
-}
-
-# Functions requiring special handling
-
-# In-place shuffle - wrapper returns shuffled input
+# Add special cases
 def shuffle(seq):
-    seq_copy = list(seq)
-    random.shuffle(seq_copy) 
-    return seq_copy
+   """Wrapper for in-place shuffle returning a copy."""
+   seq_copy = list(seq)
+   random.shuffle(seq_copy) 
+   return seq_copy
 
-rng_specs[shuffle] = RNGSpec(
-       type='seq',
-       params=lambda args: (args[0], len(args[0]))
+rng_specs.specs[shuffle] = RNGSpec(
+   type='seq',
+   params=lambda args, kwargs: (args[0], len(args[0]))
 )
 
-# Keyword arguments - wrapper converts keyword arguments into optional positional arguments
-def choices(population, weights=None, cum_weights=None, k=1): 
-    #print(population, weights, cum_weights, k)  
-    return random.choices(population, weights=weights, cum_weights=cum_weights, k=k)
-
-rng_specs[choices] = RNGSpec(
-       type='seq',
-       params=lambda args: (args[0], args[3])
-)
-
-#print(rng_specs.keys())
+if __name__ == "__main__":
+   # Test examples
+   for fun, spec in rng_specs.specs.items():
+       print(f"\nTesting {fun.__name__}:")
+       
+       # Example test cases
+       test_args = {
+           random.random: ([], {}),
+           random.uniform: ([0, 1], {}),
+           random.triangular: ([0, 1], {}),
+           random.betavariate: ([1, 1], {}),
+           random.gauss: ([0, 1], {}),
+           random.normalvariate: ([0, 1], {}),
+           random.expovariate: ([1.0], {}),
+           random.gammavariate: ([1, 1], {}),
+           random.lognormvariate: ([0, 1], {}),
+           random.vonmisesvariate: ([0, 1], {}),
+           random.paretovariate: ([1.5], {}),
+           random.weibullvariate: ([1, 1], {}),
+           random.randrange: ([10], {}),
+           random.randint: ([0, 10], {}),
+           random.choice: ([list(range(5))], {}),
+           random.choices: ([list(range(5))], {'k': 2}),
+           random.sample: ([list(range(5)), 2], {}),
+           shuffle: ([list(range(5))], {})
+       }
+       
+       args, kwargs = test_args[fun]
+       try:
+           params = spec.params(args, kwargs)
+           print(f"Type: {spec.type}")
+           print(f"Params: {params}")
+           # Test actual function
+           result = fun(*args, **kwargs)
+           print(f"Sample result: {result}")
+       except Exception as e:
+           print(f"Error testing {fun.__name__}: {e}")
