c4255a9cb93353b05e67b6f9d30615850ec9a1b9
Alberto Moraglio <albmor@gmail.com>
2025-01-05 11:35:56 +0000

    added keyword args and doc string

--- pto/core/base/distribution.py
+++ pto/core/base/distribution.py
@@ -1,47 +1,66 @@
-
 from copy import copy
 import random
-
 from .check_immutable import check_immutable
+#from pto.core.base.check_immutable import check_immutable # Use this when running the script directly
 
 class Dist():
-  
-    def __init__(self, fun, *args, val = None):
+    def __init__(self, fun, *args, val=None, **kwargs):
+        """
+        Initialize a distribution with a function, its arguments and an optional value.
+        
+        Args:
+            fun: Function to generate values
+            *args: Positional arguments for the function
+            val: Optional initial value
+            **kwargs: Keyword arguments for the function
+        """
         self.fun = fun
         self.args = tuple(copy(arg) for arg in args)
+        self.kwargs = {k: copy(v) for k, v in kwargs.items()}
         self.val = val
     
     def sample(self):
-        self.val = self.fun(*self.args)
+        """Generate a new value using the stored function and arguments."""
+        self.val = self.fun(*self.args, **self.kwargs)
 
     def repair(self, _):
+        """Regenerate the value."""
         self.sample()
     
     @check_immutable 
     def mutation(self):
+        """Create a new instance with a fresh sample."""
         offspring = copy(self)
         offspring.sample()
         return offspring
     
     @check_immutable 
     def crossover(self, other):
+        """Randomly select one of two parents."""
         return random.choice([self, other])
     
     @check_immutable 
     def convex_crossover(self, other1, other2):
+        """Randomly select one of three parents."""
         return random.choice([self, other1, other2])
     
     @check_immutable
     def distance(self, other):
+        """Binary distance metric between distributions."""
         return float(self != other)
     
     def size(self):
+        """Return fixed size of the distribution."""
         return 2
     
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.fun.__name__}, {self.args}, val={self.val})' 
+        """String representation of the distribution."""
+        return (f'{self.__class__.__name__}({self.fun.__name__}, '
+                f'{self.args}, {self.kwargs}, val={self.val})')
     
     def __eq__(self, other):
+        """Check equality based on function, arguments, and value."""
         return (self.fun.__name__ == other.fun.__name__ and 
                 self.args == other.args and 
+                self.kwargs == other.kwargs and 
                 self.val == other.val)
